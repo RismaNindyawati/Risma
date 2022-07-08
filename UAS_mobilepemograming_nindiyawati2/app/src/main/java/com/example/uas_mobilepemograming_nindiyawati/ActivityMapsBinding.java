cee88c875d9abba56c2ba21c736192cbedf07619
unknown <rismanindy17@gmail.com>
2022-07-08 15:30:58 +0700

    UAS RISMA

---
+++ UAS_mobilepemograming_nindiyawati2/app/src/main/java/com/example/uas_mobilepemograming_nindiyawati/ActivityMapsBinding.java
@@ -0,0 +1,13 @@
+package com.example.uas_mobilepemograming_nindiyawati;
+
+import android.view.LayoutInflater;
+
+public class ActivityMapsBinding {
+    public static ActivityMapsBinding inflate(Object inflater) {
+        return null;
+    }
+
+    public int getRoot() {
+        return 0;
+    }
+}
